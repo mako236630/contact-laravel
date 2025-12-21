16b2bb1312c0757c46065850a740881485b2c2d1
mako <immako.236630@gmail.com>
2025-12-21 09:02:38 +0900

    contact・confirm・thanks画面完成

--- src/resources/views/contact/confirm.blade.php
+++ src/resources/views/contact/confirm.blade.php
@@ -1,64 +1,85 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>お問い合わせフォーム確認ページ</title>
-</head>
-    @extends('layouts.app')
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/confirm.css') }}">
+@endsection
 
-    @section('content')
-    <div class="container">
-        <h2 class="content-title">Confirm</h2>
-        <form action="{{ url('/thanks') }}" method="post">
-            @csrf
-            <table class="confirm-table">
-                <tr>
-                    <th>お名前</th>
-                    <td>{{ $data['first_name'] }} {{ $data['last_name'] }}</td>
-                </tr>
-                <tr>
-                    <th>性別</th>
-                    <td>{{ $data['gender'] == '1' ? '男性' : ($data['gender'] == '2' ? '女性' : 'その他') }}</td>
-                </tr>
-                <tr>
-                    <th>メールアドレス</th>
-                    <td>{{ $data['email'] }}</td>
-                </tr>
-                <tr>
-                    <th>電話番号</th>
-                    <td>{{ $data['tel1'] . $data['tel2'] . $data['tel3'] }}</td>
-                </tr>
-                <tr>
-                    <th>住所</th>
-                    <td>{{ $data['address'] }}</td>
-                </tr>
-                <tr>
-                    <th>建物名</th>
-                    <td>{{ $data['building'] }}</td>
-                </tr>
-                <tr>
-                    <th>お問い合わせの種類</th>
-                    <td>{{ $data['category_content'] }}</td>
-                </tr>
-                <input type="hidden" name="first_name" value="{{ $data['first_name'] }}">
-                <input type="hidden" name="last_name" value="{{ $data['last_name'] }}">
-                <input type="hidden" name="gender" value="{{ $data['gender'] }}">
-                <input type="hidden" name="email" value="{{ $data['email'] }}">
-                <input type="hidden" name="tel1" value="{{ $data['tel1'] }}">
-                <input type="hidden" name="tel2" value="{{ $data['tel2'] }}">
-                <input type="hidden" name="tel3" value="{{ $data['tel3'] }}">
-                <input type="hidden" name="address" value="{{ $data['address'] }}">
-                <input type="hidden" name="building" value="{{ $data['building'] }}">
-                <input type="hidden" name="category_id" value="{{ $data['category_id'] }}">
-                <input type="hidden" name="detail" value="{{ $data['detail'] }}">
-            </table>
+@section('content')
+<div class="confirm__container">
+    <h2 class="confirm__title">Confirm</h2>
 
-            <div style="text-align: center; margin-top: 30px;">
-                <button type="submit" class="btn">送信</button>
-                <button type="submit" name="back" value="true" class="btn-secondary">修正</button>
-            </div>
-        </form>
-    </div>
-    @endsection
+    <form action="{{ route('contact.store') }}" method="post">
+        @csrf
+        <table class="confirm-table">
+            <tr class="confirm-table__row">
+                <th class="confirm-table__header">お名前</th>
+                <td class="confirm-table__text">
+                    {{ $data['first_name'] }} {{ $data['last_name'] }}
+                </td>
+            </tr>
+            <tr class="confirm-table__row">
+                <th class="confirm-table__header">性別</th>
+                <td class="confirm-table__text">
+                    {{ $data['gender'] == '1' ? '男性' : ($data['gender'] == '2' ? '女性' : 'その他') }}
+                </td>
+            </tr>
+
+            <tr class="confirm-table__row">
+                <th class="confirm-table__header">メールアドレス</th>
+                <td class="confirm-table__text">{{ $data['email'] }}
+
+                </td>
+            </tr>
+
+            <tr class="confirm-table__row">
+                <th class="confirm-table__header">電話番号</th>
+                <td class="confirm-table__text">{{ $data['tel1'] . $data['tel2'] . $data['tel3'] }}
+
+                </td>
+            </tr>
+
+            <tr class="confirm-table__row">
+                <th class="confirm-table__header">住所</th>
+                <td class="confirm-table__text">{{ $data['address'] }}
+                </td>
+            </tr>
+
+            <tr class="confirm-table__row">
+                <th class="confirm-table__header">建物名</th>
+                <td class="confirm-table__text">{{ $data['building'] }}
+                </td>
+            </tr>
+
+            <tr class="confirm-table__row">
+                <th class="confirm-table__header">お問い合わせの種類</th>
+                <td class="confirm-table__text">{{ $data['category_content'] }}
+                </td>
+            </tr>
+
+            <tr class="confirm-table__row">
+                <th class="confirm-table__header">お問い合わせの内容</th>
+                <td class="confirm-table__text">{{ $data['detail'] }}
+                </td>
+            </tr>
+
+            <input type="hidden" name="first_name" value="{{ $data['first_name'] }}">
+            <input type="hidden" name="last_name" value="{{ $data['last_name'] }}">
+            <input type="hidden" name="gender" value="{{ $data['gender'] }}">
+            <input type="hidden" name="email" value="{{ $data['email'] }}">
+            <input type="hidden" name="tel1" value="{{ $data['tel1'] }}">
+            <input type="hidden" name="tel2" value="{{ $data['tel2'] }}">
+            <input type="hidden" name="tel3" value="{{ $data['tel3'] }}">
+            <input type="hidden" name="address" value="{{ $data['address'] }}">
+            <input type="hidden" name="building" value="{{ $data['building'] }}">
+            <input type="hidden" name="category_id" value="{{ $data['category_id'] }}">
+            <input type="hidden" name="detail" value="{{ $data['detail'] }}">
+
+        </table>
+
+        <div class="confirm-form__button">
+            <button class="btn-submit" type="submit">送信</button>
+            <button class="btn-back" type="submit" name="back" value="back">修正</button>
+        </div>
+    </form>
+</div>
+@endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -17,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/", [ContactController::class, "index"])->name("contact.index");
 Route::post("/confirm", [ContactController::class, "confirm"])->name("contact.confirm");
-Route::post("/thanks", [ContactController::class, "store"]);
+Route::post("/thanks", [ContactController::class, "store"])->name("contact.store");
 Route::get("/thanks", [ContactController::class, "thanks"])->name("contact.thanks");
